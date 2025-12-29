12e94074399c9b6d3fdc2b37e9ceb54dd43ad969
Pritul-Raut <139654690+Pritul-Raut@users.noreply.github.com>
2025-12-30 00:13:01 +0530

    allowed cors

--- server/server.js
+++ server/server.js
@@ -23,25 +23,25 @@ const allowedOrigins = [
 ];
 
 app.use(
-  // cors({
-  //   origin: process.env.CLIENT_URL,
-  //   methods: ["GET", "POST", "DELETE", "PUT"],
-  //   allowedHeaders: ["Content-Type", "Authorization"],
-  // })
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
+  // cors({
+  //   origin: function (origin, callback) {
+  //     // Allow requests with no origin (like mobile apps or curl requests)
+  //     if (!origin) return callback(null, true);
+      
+  //     if (allowedOrigins.indexOf(origin) === -1) {
+  //       var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+  //       return callback(new Error(msg), false);
+  //     }
+  //     return callback(null, true);
+  //   },
+  //   methods: ["GET", "POST", "DELETE", "PUT"],
+  //   allowedHeaders: ["Content-Type", "Authorization"],
+  // })
 );
 
 app.use(express.json());
